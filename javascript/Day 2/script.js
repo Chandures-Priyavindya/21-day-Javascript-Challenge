b374798e3233d8f8130d529363dae409fe385ae2
Chandures-Priyavindya <145960853+Chandures-Priyavindya@users.noreply.github.com>
2025-03-16 09:03:22 +0530

    Update script.js

--- javascript/Day 2/script.js
+++ javascript/Day 2/script.js
@@ -2,3 +2,8 @@ function isPalindrome(str){
   return str === str.split('').reverse().join('');
 }
 console.log(isPalindrome("madam"));
+
+function reverseString(str){
+  return str.split('').reverse().join('');
+}
+console.log(reverseString("task"));
